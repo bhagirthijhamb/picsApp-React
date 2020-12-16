d01f08e8bcfc39bc2da642e7f00b7622d47582dd
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-16 11:34:09 -0500

    imllemeted Images fetching from Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './Searchbar'
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
+    console.log(term);
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID I3-4CZRBsPwTlUUqjaBQZfPyBSwP3XEi5xcSXXSyArM'
+      }
+    })
+    // console.log(response.data.results);
+    this.setState({ images: response.data.results })
+  }
+
+  render(){
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -10,7 +10,8 @@ class SearchBar extends React.Component{
   onFormSubmit = (e) => {
     e.preventDefault();
 
-    console.log(this.state.term);
+    // console.log(this.state.term);
+    this.props.onSubmit(this.state.term)
   }
 
   render(){
