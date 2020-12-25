a43aadb415b88f5c4e10b5abedae02a04e8bcc6c
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-25 17:55:05 -0500

    added code for axios request

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import SearchBar from './Searchbar'
 import unsplash from './../api/unsplash';
 import ImageList from './ImageList';
+import axios from 'axios';
 
 class App extends React.Component {
   state = { images: [] }
 
   onSearchSubmit = async (term) => {
     console.log(term);
+    // axios.get('https://api.unsplash.com/search/photos', {
+    //   params: { query: term },
+    //   headers: {
+    //     Authorization: 'Client-ID I3-4CZRBsPwTlUUqjaBQZfPyBSwP3XEi5xcSXXSyArM'
+    //   }
+    // }).then(response => { console.log(response.data.results)})
+
     const response = await unsplash.get('/search/photos', {
-      params: {
-        query: term
-      }
+      params: { query: term }
     })
     // console.log(response.data.results);
     this.setState({ images: response.data.results })
--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// App challenges
+// - Need to get a search term from the user
+// - Need to use that search term  to make a request to an outside api and fetch data
+// - Need to take the fetched images and show them on the sreen in  a list.
